c90af50c5b72c06a47646d325bcbd6a81744802e
Matias Jørgensen <matjorrub@gmail.com>
2026-03-24 08:53:45 +0100

    feat: Enhance Header component with new icons and layout adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,53 @@
 import { NavLink, Link } from "react-router";
 import Logo from "../assets/logo.svg";
+import Plane from "../assets/icons/paper-plane.svg";
+import Tlf from "../assets/icons/tlf.svg";
+import User from "../assets/icons/user.svg";
 import "./Header.scss";
 export default function Header() {
   return (
-    <div>
-      <header className=" section_wrapper header">
-        <Link to={"/"}>
-          <img src={Logo} alt="" />
-        </Link>
-        <nav className="header_nav">
-          <ul className="header_nav_ul">
+    <>
+      <section className="header_top_back">
+        <div className="section_wrapper header_top">
+          <ul className="header_top_ul">
             <li>
-              <NavLink>Boliger til salg</NavLink>
+              <img src={Plane} alt="" />
+              <a href="#">4000@dinmaegler.com</a>
             </li>
             <li>
-              <NavLink>Mæglere</NavLink>
+              <img src={Tlf} alt="" />
+              <a href="#">+45 7070 4000</a>
             </li>
             <li>
-              <NavLink>Mine favoritter</NavLink>
-            </li>
-            <li>
-              <NavLink>Kontakt</NavLink>
+              <img src={User} alt="" />
+              <a href="#">Log ind</a>
             </li>
           </ul>
-        </nav>
-      </header>
-    </div>
+        </div>
+      </section>
+      <section>
+        <header className=" section_wrapper header">
+          <Link to={"/"}>
+            <img src={Logo} alt="" />
+          </Link>
+          <nav className="header_nav">
+            <ul className="header_nav_ul">
+              <li>
+                <NavLink>Boliger til salg</NavLink>
+              </li>
+              <li>
+                <NavLink>Mæglere</NavLink>
+              </li>
+              <li>
+                <NavLink>Mine favoritter</NavLink>
+              </li>
+              <li>
+                <NavLink>Kontakt</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </section>
+    </>
   );
 }
